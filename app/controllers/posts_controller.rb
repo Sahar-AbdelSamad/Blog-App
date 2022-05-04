9d28541ef5e6c3448caf0383f66754183b6e58c6
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-04 20:18:42 +0300

    Update index and show actions of Posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.where(author_id: @user.id).order(created_at: :desc)
+  end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.where(post_id: @post.id).order(created_at: :desc)
+  end
 end
