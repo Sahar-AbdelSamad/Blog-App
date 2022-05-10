6001f2da88b764e5f9f6e2371d1c675a16449759
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-05-10 13:21:05 +0300

    Remove users file from controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: { 
-    sign_in: 'login', 
-    sign_out: 'logout', 
-    password: 'secret', 
-    confirmation: 'verification', 
-    unlock: 'unblock', 
-    registration: 'register', 
-    sign_up: 'sign_up'
-  }
-  # devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', passwords: 'users/passwords'} 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'sign_up' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   devise_scope :user do
-    get '/sign_out', to: 'users/sessions#destroy'
-    get '/sign_up', to: 'users/registrations#new'
-    get '/login', to: 'users/sessions#new'
+    get '/logout', to: 'devise/sessions#destroy'
   end
 
   resources :users, only: [:index, :show] do
